6c0c656896e36a589bba51918b16ff40ef382359
xinyithepotato <xinyi395@gmail.com>
2026-03-14 22:24:25 +0800

    push

--- lib/puter.hosting.ts
+++ lib/puter.hosting.ts
@@ -21,7 +21,11 @@ export const getOrCreateHostingConfig = async (): Promise<HostingConfig | null> 
     try {
         const created = await puter.hosting.create(subdomain, '.');
 
-        return { subdomain:  created.subdomain };
+        const record = { subdomain:  created.subdomain };
+
+        await puter.kv.set(HOSTING_CONFIG_KEY, record);
+
+        return record;
     } catch (e) {
         console.warn('could not find subdomain', e);
         return null;
